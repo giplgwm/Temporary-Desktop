3ccd5073e13cdc0545dc7601f2627bad9b6a4b8f
gip <98342753+giplgwm@users.noreply.github.com>
2025-07-12 22:06:12 -0400

    Move to system tray
    Version 2.0 - the program lives down in the tray now since we do not need a whole window that just gets in the way.

--- README.md
+++ README.md
@@ -3,17 +3,17 @@ This is a program for temporarily changing the folder that windows uses for the 
 
 ![Preview Image](temp_desktop_preview.png "Preview Image")
 
-It works by simply editing the Registry key that sets your desktop folder and then copying all desktop shortcuts to a backup folder to hide them temporarily. 
+It lives in the system tray and works by simply editing the Registry key that sets your desktop folder and optionally copying all public desktop shortcuts to a backup folder to hide them temporarily. 
 
 ## Usage
 1. Install [.NET 8](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
 2. Download the [latest release](https://github.com/giplgwm/Temporary-Desktop/releases/latest) of Temp Desktop
 3. Extract the folder
-4. Run `Temp Desktop.exe`
-5. Press `Change Desktop Folder` and select the folder you'd like to use. Consider adding a shortcut to `Temp Desktop.exe` in the folder you'll be using as your desktop to make it more convenient to switch back.
-6. You will be signed out of Windows - sign back in and the Desktop folder will be changed.
-7. When you're done working in the temporary desktop folder, re-open the application and choose `Reset Desktop Folder`
+4. Run `Temp Desktop.exe` and the Temp Desktop icon will appear in your system tray
+5. Right click the icon in the tray to open the menu
+6. Press `Change Desktop Folder` and select the folder you'd like to use. Consider adding a shortcut to `Temp Desktop.exe` in the folder you'll be using as your desktop to make it more convenient to switch back.
+7. When you're done working in the temporary desktop folder, choose `Reset Desktop Folder` or simply click `Exit` with the "Reset desktop on app close" option enabled (it is by default).
 
 ### Notes
-- When changing the desktop folder your desktop shortcuts will be moved from C:\Users\Public\Desktop to C:\Users\Public_Desktop_Backup. This keeps them from showing on the new desktop screen, they are moved back to the appropriate folder when `Reset Desktop Folder` is selected.
+- If hide shortcuts is enabled in options, public desktop shortcuts will be moved from C:\Users\Public\Desktop to C:\Users\Public\Desktop_backup. This keeps them from showing on the new desktop screen, they are moved back to the appropriate folder when the desktop is reset either by pressing the button or closing the application with the reset on close option enabled.
 - The program requires admin access due to the above note, since adding files to C:\Users\Public\Desktop requires elevated priveledges.
--- Temp Desktop/Program.cs
+++ Temp Desktop/Program.cs
@@ -1,17 +1,296 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
 namespace Temp_Desktop
 {
     internal static class Program
     {
+
+        static string originaldesktop;
+        static string currentdesktop;
+        static ToolStripLabel currentdesktoplabel;
+        static bool hideShortcuts;
+        static bool resetOnExit = true;
+        static NotifyIcon trayicon;
+        static string registrySaveLocation = "SOFTWARE\\gip\\TempDesktop";
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(String[] args)
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+            if (args.Contains("--self-restart"))
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(registrySaveLocation, true))
+                {
+                    if (key != null)
+                    {
+                        originaldesktop = key.GetValue("originaldesktop").ToString();
+                        Trace.WriteLine("Loaded from REGISTRY");
+                    }
+                    else
+                    {
+                        using (RegistryKey key2 = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders", true))
+                        {
+                            originaldesktop = key2.GetValue("Desktop").ToString();
+                        }
+                        key.SetValue("originaldesktop", originaldesktop);
+                    }
+                }
+            } else
+            {
+                Trace.WriteLine("Loaded default windows location");
+                using (RegistryKey key2 = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders", true))
+                {
+                    originaldesktop = key2.GetValue("Desktop").ToString();
+                }
+            }
+            currentdesktop = originaldesktop;
+            
+            if (originaldesktop == null)
+            {
+                Application.Exit();
+            }
+
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            currentdesktoplabel = new ToolStripLabel();
+            currentdesktoplabel.Text = "Current Desktop: " + currentdesktop;
+            menu.Items.Add(currentdesktoplabel);
+
+            ToolStripMenuItem changeItem = new ToolStripMenuItem();
+            changeItem.Text = "Change Desktop Folder";
+            changeItem.Click += new System.EventHandler(changeClicked);
+            menu.Items.Add(changeItem);
+
+            ToolStripMenuItem resetItem = new ToolStripMenuItem();
+            resetItem.Text = "Reset Desktop Folder";
+            resetItem.ToolTipText = "Original Desktop: " + originaldesktop;
+            resetItem.Click += new System.EventHandler(resetClicked);
+            menu.Items.Add(resetItem);
+
+            // Options menu
+            ToolStripMenuItem optionsMenu = new ToolStripMenuItem("Options");
+            ToolStripMenuItem hideShortcutsItem = new ToolStripMenuItem("Hide all desktop shortcuts (requires admin)");
+            hideShortcutsItem.CheckOnClick = true;
+            hideShortcutsItem.ToolTipText = "Should Public Desktop Shortcuts be hidden? This includes system shortcuts like Recycle Bin and Edge as well as Programs installed for all users. Default is no to allow program to run without admin privileges.";
+            if (IsAdministrator())
+            {
+                hideShortcuts = true;
+            } else
+            {
+                hideShortcuts = false;
+            }
+            hideShortcutsItem.Checked = hideShortcuts;
+            hideShortcutsItem.CheckedChanged += hideShortcuts_CheckedChanged;
+
+            ToolStripMenuItem resetOnCloseItem = new ToolStripMenuItem("Reset desktop on app close");
+            resetOnCloseItem.CheckOnClick = true;
+            resetOnCloseItem.Checked = resetOnExit;
+            resetOnCloseItem.ToolTipText = "Should the desktop be reset when the program is closed? Default is yes to prevent users forgetting to switch back to the default and having issues.";
+            resetOnCloseItem.CheckedChanged += resetExit_CheckedChanged;
+
+            optionsMenu.DropDownItems.Add(hideShortcutsItem);
+            optionsMenu.DropDownItems.Add(resetOnCloseItem);
+            menu.Items.Add(optionsMenu);
+
+
+            ToolStripMenuItem closeItem = new ToolStripMenuItem();
+            closeItem.Text = "Exit";
+            closeItem.Click += new System.EventHandler(exitClicked);
+            menu.Items.Add(closeItem);
+
+            IContainer container = new Container();
+
+            trayicon = new NotifyIcon(container);
+            string resourceName = "Temp_Desktop.desktop_folder.ico";
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                if (stream != null)
+                {
+                    Icon icon = new Icon(stream);
+                    trayicon.Icon = icon;
+                }
+            }
+            trayicon.Text = "Temporary Desktop";
+            trayicon.Visible = true;
+            trayicon.ContextMenuStrip = menu;
+
+            Application.Run();
+        }
+
+        private static void changeClicked(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.Title = "Select the folder to use as your desktop";
+            dialog.InitialDirectory = currentdesktop;
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                SetDesktopRegKey(dialog.FileName);
+                if (hideShortcuts)
+                {
+                    removeShortcuts();
+                }
+                refreshDesktop();
+            }
+        }
+
+        private static void refreshDesktop()
+        {
+            // Find the taskbar window (Shell_TrayWnd)
+            IntPtr hWnd = FindWindow("Shell_TrayWnd", null);
+
+            // WM_USER + 436 is the message to close the Explorer shell
+            const uint WM_USER = 0x0400;
+            PostMessage(hWnd, WM_USER + 436, IntPtr.Zero, IntPtr.Zero);
+
+            // Wait for Explorer to fully exit
+            while (FindWindow("Shell_TrayWnd", null) != IntPtr.Zero)
+            {
+                Thread.Sleep(750);
+            }
+
+            // Restart Explorer
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                UseShellExecute = true
+            });
+        }
+
+        private static void SetDesktopRegKey(string newDesktop)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders", true))
+            {
+                if (key != null) 
+                {
+                    key.SetValue("Desktop", newDesktop);
+                    currentdesktop = newDesktop;
+                    currentdesktoplabel.Text = "Current Desktop: " + newDesktop;
+                }
+            }
+        }
+
+        private static void resetClicked(object sender, EventArgs e)
+        {
+            SetDesktopRegKey(originaldesktop);
+            currentdesktop = originaldesktop;
+            currentdesktoplabel.Text = "Current Desktop: " + originaldesktop;
+            returnShortcuts();
+            refreshDesktop();
+        }
+
+        private static void exitClicked(object sender, EventArgs e)
+        {
+            trayicon.Visible = false;
+            if (resetOnExit && currentdesktop != originaldesktop) { resetClicked(sender, e); }
+            Application.Exit();
+        }
+
+        private static void CopyAllFiles(string from, string to, bool deleteFrom = false)
+        {
+            DirectoryInfo from_dir = new DirectoryInfo(from);
+            if (!Directory.Exists(to))
+            {
+                Directory.CreateDirectory(to);
+            }
+
+            foreach (FileInfo file in from_dir.GetFiles())
+            {
+                file.CopyTo(to + "\\" + file.Name);
+                file.Delete();
+            }
+            if (deleteFrom)
+            {
+                from_dir.Delete();
+            }
+        }
+
+        private static void removeShortcuts()
+        {
+            string shortcutDir = "C:\\Users\\Public\\Desktop";
+            string shortcutBackupDir = "C:\\Users\\Public\\Desktop_backup";
+            CopyAllFiles(shortcutDir, shortcutBackupDir);
+
+        }
+
+        private static void returnShortcuts()
+        {
+            string shortcutDir = "C:\\Users\\Public\\Desktop";
+            string shortcutBackupDir = "C:\\Users\\Public\\Desktop_backup";
+            if (Directory.Exists(shortcutBackupDir))
+            {
+                CopyAllFiles(shortcutBackupDir, shortcutDir, true);
+            }
+        }
+
+        private static void hideShortcuts_CheckedChanged(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            hideShortcuts = item.Checked;
+            if (hideShortcuts)
+            {
+                RestartAsAdmin();
+            }
+            if (!hideShortcuts)
+            {
+                returnShortcuts();
+            }
+        }
+
+        private static void resetExit_CheckedChanged(object sender, EventArgs e)
+        {
+            ToolStripMenuItem reset = sender as ToolStripMenuItem;
+            resetOnExit = reset.Checked;
+            Trace.WriteLine(resetOnExit);
+        }
+
+        static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        static void RestartAsAdmin()
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(registrySaveLocation, true))
+            {
+                key.SetValue("originaldesktop", originaldesktop);
+            }
+            trayicon.Visible = false;
+            var psi = new ProcessStartInfo
+            {
+                FileName = Environment.ProcessPath,
+                UseShellExecute = true,
+                Verb = "runas",
+                Arguments = "--self-restart"
+            };
+            Process.Start(psi);
+            Environment.Exit(0);
         }
     }
 }
